49a6c39f41bad703849cbefa736e1d5dc72204f0
Givi Odikadze <givi.odikadze@gmail.com>
2021-11-15 09:54:09 -0800

    x

--- src/components/exercise/hint.tsx
+++ src/components/exercise/hint.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {HintEntity} from "../../models/hint.entity";
 import Latex from "react-latex-next";
 import {Box, Divider, VStack} from "@chakra-ui/react";
+import {QuestionIcon} from "@chakra-ui/icons";
 
 interface Props {
     hint: HintEntity
@@ -14,7 +15,8 @@ export const Hint = ({hint}: Props) => {
 
     return <VStack align={'stretch'}>
         <Divider mt={'0px !important'}/>
-        <Box p={4}>
+        <Box p={4} position={'relative'}>
+            <QuestionIcon color={'orange'} position={'absolute'} top={0} right={2}/>
             <Latex>{hint.content}</Latex>
         </Box>
     </VStack>
